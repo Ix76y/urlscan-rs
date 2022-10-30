0a65a429bc5845e7e670689920d34c319af6289e
Iz76y <is.hoekstein@gmail.com>
2022-10-30 23:39:26 +0000

    UrlScanClient and folder structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
+pub mod api;
+
+#[derive(Clone)]
+pub struct UrlScanClient {
+    api_key: String,
+    domain: String,
+    endpoint: String,
+}
+
+impl UrlScanClient {
+    pub fn new(api_key: &str) -> Self {
+        //! Creates a new URLScan.io API client 
+        //! 
+        //! ## Example usage
+        //! ```rust
+        //! use urlscan::UrlScanClient;
+        //! let urlscan_client = UrlScanClient::new("API_KEY");
+        //! ```
+        UrlScanClient {
+            api_key: api_key.into(),
+            domain: "https://urlscan.io/".into(),
+            endpoint: "api/v1/".into()
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
