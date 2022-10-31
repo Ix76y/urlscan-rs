39ddbb0736d8064b1873cdca7316708cac7c76f1
Iz76y <is.hoekstein@gmail.com>
2022-10-31 09:32:32 +0000

    Dependencies & scan API

---
+++ src/api/submission/mod.rs
@@ -0,0 +1,11 @@
+use crate::UrlScanClient;
+
+impl UrlScanClient {
+    pub fn scan_url(&self, url: &str) {
+        let request_url = format!("{}{}scan/", &self.domain, &self.endpoint);
+    }
+
+    pub fn scan_urls(&self, urls: [&str]) {
+
+    }
+}
